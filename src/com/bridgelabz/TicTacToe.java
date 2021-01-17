6c03af1c6df88fef64bb02eb59565428afd40dab
ajay812998 <ajay812998@gmail.com>
2021-01-17 16:17:30 +0530

    Added : Welcome to TicTacToe Problem

---
+++ src/com/bridgelabz/TicTacToe.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class TicTacToe {
+	static char[] board;
+	public static void main(String[] args) {
+		board=new char[10];
+	System.out.println("Welcome to TicTacToe");
+   }
+}
