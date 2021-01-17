cff6a3ce3ffb6a10d5444c0bd854b705eb8cd03e
ajay812998 <ajay812998@gmail.com>
2021-01-17 16:24:21 +0530

    Added : Creating a TicTacToe Board

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -5,5 +5,13 @@ public class TicTacToe {
 	public static void main(String[] args) {
 		board=new char[10];
 	System.out.println("Welcome to TicTacToe");
+	
+	populateBoard();
+}
+	
+	static void populateBoard() {
+		for (int i = 1; i < 10; i++) {
+			board[i] = ' ';
+		}
    }
 }
