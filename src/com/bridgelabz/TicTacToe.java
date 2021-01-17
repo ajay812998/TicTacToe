f742f3cb68b3339961913743390f31f9eb797189
ajay812998 <ajay812998@gmail.com>
2021-01-17 17:55:57 +0530

    Added : Ability to Move To Desired Location in Board

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -4,26 +4,44 @@ import java.util.Scanner;
 
 public class TicTacToe {
 	static char[] board = new char[10];
+	static char player = ' ';
 
 	public static void main(String[] args) {
-		char player = ' ';
+
 		char computer = ' ';
 		System.out.println("Welcome to TicTacToe");
 		populateBoard();
 		player = selectXorO();
-		if (player == 'X') {
-			computer = 'O';
-		} else {
-			computer = 'X';
-		}
+		computer = player == 'X' ? 'O' : 'X';
 		System.out.println(player + " is player " + computer + " is computer");
-
 		showBoard();
+		makeaMove();
+	}
+
+	private static void makeaMove() {
+		Scanner scan = new Scanner(System.in);
+		while (true) {
+			System.out.println("Enter a slot to place " + player);
+			int slot = scan.nextInt();
+			if (slot >= 1 && slot <= 9) {
+				if (board[slot] == ' ') {
+					board[slot] = player;
+					break;
+				} else {
+					System.out.println("slot already taken,Enter again");
+				}
+			} else {
+				System.out.println("Invalid choice.Enter again");
+			}
+		}
+		showBoard();
+
 	}
 
 	private static void showBoard() {
 		for (int i = 1; i < board.length; i = i + 3) {
-			System.out.println("| " + board[i] + " | " + board[i + 1] + " | " + board[i + 2] + " |");
+			System.out.println("|" + board[i] + " |" + board[i + 1] + " |" + board[i + 2] + " |");
+			System.out.println("-----------");
 		}
 	}
 
@@ -32,11 +50,9 @@ public class TicTacToe {
 		Scanner scan = new Scanner(System.in);
 		while (true) {
 			System.out.println("Select either X or O");
-			player = scan.next().charAt(0);
-			if (player == 'x' || player == 'X') {
-				return 'X';
-			} else if (player == 'o' || player == 'O') {
-				return 'O';
+			player = scan.next().toUpperCase().charAt(0);
+			if (player == 'X' || player == 'O') {
+				return player;
 			} else {
 				System.out.println("Invalid choice,Enter again");
 				continue;
